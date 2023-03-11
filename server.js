0ebdda0e6cc522c0f8c06be6c031fe87916787cf
Ken Azura <kenneth.azura@gmail.com>
2023-03-11 13:46:38 +0800

    [GPC] Debugging Summary: Debugging URL strings in production server

--- server.js
+++ server.js
@@ -38,6 +38,7 @@ server.get(`${process.env.API_ROUTE}/job-categories`, (req, res) => {
 });
 
 server.get('/', (req, res) => {
+  console.log(`${process.env.DOMAIN}${process.env.API_ROUTE}/job-categories?count=6`);
   fetch(`${process.env.DOMAIN}${process.env.API_ROUTE}/job-categories?count=6`)
     .then(async(oResponse) => {
       const aJobCategories = await oResponse.json();
