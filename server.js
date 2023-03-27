5fcf47f80665ba6174cbf82e0c17574a0297f945
Ken Azura <kenneth.azura@gmail.com>
2023-03-27 13:55:30 +0800

    [GPC] Job Posting Summary: Completed Dynamic Job Posting Page Changes:  - Created dataset and API for retrieving job posting content based on    parameters  - Finished hero, description, responsibilities, and qualifications sections  - Fixed navbar & footer links to work in the job posting page

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+
 if (process.env.ENVIRONMENT !== 'production') {
   const dotenv = require('dotenv');
   dotenv.config();
@@ -11,9 +12,9 @@ server.set('view engine', 'ejs');
 
 // #############################################################################
 // Logs all request paths and method
-server.use(function (req, res, next) {
-  res.set('x-timestamp', Date.now())
-  res.set('x-powered-by', 'cyclic.sh')
+server.use(function(req, res, next) {
+  res.set('x-timestamp', Date.now());
+  res.set('x-powered-by', 'cyclic.sh');
   console.log(`[${new Date().toISOString()}] ${req.ip} ${req.method} ${req.path}`);
   next();
 });
@@ -27,7 +28,6 @@ if (process.env.ENVIRONMENT === 'local') {
 server.get(`${process.env.API_ROUTE}/job-categories`, (req, res) => {
   const sJobCategoriesDataURL = `${process.env.DOMAIN}${process.env.ASSET_LINK}/data/job-categories.json`;
 
-  console.log(sJobCategoriesDataURL);
   fetch(sJobCategoriesDataURL, { method: 'Get' })
     .then((oResponse) => oResponse.json())
     .then((oJobCategories) => {
@@ -35,6 +35,25 @@ server.get(`${process.env.API_ROUTE}/job-categories`, (req, res) => {
       const iStart = (req.query.start) ? parseInt(req.query.start, 10) : 0;
       const iCount = (req.query.count) ? iStart + parseInt(req.query.count, 10) : iStart + 3;
       return res.send(aJobCategories.slice(iStart, iCount));
+    });
+});
+
+server.get(`${process.env.API_ROUTE}/job-details`, (req, res) => {
+  const sJobDataURL = `${process.env.DOMAIN}${process.env.ASSET_LINK}/data/job-details.json`;
+
+  fetch(sJobDataURL, { method: 'Get' })
+    .then((oResponse) => oResponse.json())
+    .then((oJobDetails) => {
+      const aJobs = oJobDetails.jobDetails;
+      const iLength = aJobs.length;
+      let oJob = '';
+
+      for (let iCount = 0; iCount < iLength; iCount += 1) {
+        if (aJobs[iCount].title.replace(/[^a-zA-Z]/g, '').toLowerCase().includes(req.query.title.replace(/[^a-zA-Z]/g, ''))) oJob = aJobs[iCount];
+      }
+
+      if (oJob === '') return res.send(`Error! "${req.query.title}" not found.`);
+      return res.send(oJob);
     });
 });
 
@@ -47,8 +66,11 @@ server.get('/', (req, res) => {
 });
 
 server.get('/job/:jobTitle', (req, res) => {
-  console.log(req.params);
-  res.render('job-posting.ejs', { assetLink: process.env.ASSET_LINK });
+  fetch(`${process.env.DOMAIN}${process.env.API_ROUTE}/job-details?title=${req.params.jobTitle}`)
+    .then(async(oResponse) => {
+      const oJobDetails = await oResponse.json();
+      res.render('job-posting.ejs', { jobDetails: oJobDetails, assetLink: process.env.ASSET_LINK });
+    });
 });
 
 server.listen(port, () => {
